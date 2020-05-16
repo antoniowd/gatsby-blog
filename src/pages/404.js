bc70071f652d1d876d59081386db5766b981b0f1
Antonio Martin <tonitim13@gmail.com>
2020-05-16 12:11:03 -0300

    nuevo diseno de la pagina 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout siteTitle="404: Pagina no encontrada">
+    <br />
+    <h1 
+      className="title is-1 has-text-centered has-text-warning"
+      style={{fontSize: '6rem'}}
+    >
+      404
+    </h1>
+    <p className="title is-3 has-text-centered">No hemos podido encontrar esta página.</p>
+    <p className="has-text-centered">Te dejo aquí <Link to="/">las últimas publicaciones</Link> para que te pongas al día.</p>
   </Layout>
 )
 
