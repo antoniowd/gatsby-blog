31ea21f9b7bed5c83d9cc3854fad579bda3b3c53
Antonio Martin <tonitim13@gmail.com>
2020-05-20 09:42:41 -0300

    mas optimizacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,19 +25,21 @@ const Layout = ({ children, siteTitle, siteDescription, siteKeywords }) => {
 
       <footer className="footer">
         <div className="content has-text-centered">
-          <a target="_BLANK" aria-label="Acceder a twitter" rel="noopener noreferrer" href="https://twitter.com/antoniowebdev" className="button is-text">
+          <a target="_BLANK" aria-label="Acceder a twitter" rel="noopener noreferrer" href="https://twitter.com/antoniowebdev" className="button is-text" style={{ marginRight: '25px' }}>
             <span className="icon">
-              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
             </span>
           </a>
           <a target="_BLANK" aria-label="Acceder a reddit" rel="noopener noreferrer" href="https://www.reddit.com/user/antonio-martin" className="button is-text">
             <span className="icon">
-              <FontAwesomeIcon icon={faReddit} />
+              <FontAwesomeIcon icon={faReddit} size="2x" />
             </span>
           </a>
+          <br />
+          <br />
           <div className="is-size-7">
             {new Date().getFullYear()} Antonio Martin | Todos los derechos reservados
-            </div>
+          </div>
         </div>
       </footer>
     </>
